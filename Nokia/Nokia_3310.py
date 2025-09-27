8917aea678036ec7146476272b5c5a21502f7898
Mayorking14 <edwardmichael953@gmail.com>
2025-09-28 00:04:07 +0100

    progress

--- Nokia/Nokia_3310.py
+++ Nokia/Nokia_3310.py
@@ -1,21 +1,65 @@
 class PhoneBook:
     def __init__(self):
-        self.search = None
-        self.service_Nos = None
-        self.add_Name = []
-        self.erase = None
-        self.edit = None
-        self.assign_Tone = None
-        self.send_B_Card = None
-        self.options = []
-        self.speed_dials = []
-        self.voice_Tags = []
+        self.contact = {}
 
-    def set_add_Name(self, name):
-        self.add_Name.append(name)
+    def is_empty(self):
+        return self.contact
 
-    def get_add_Name(self):
-        return self.add_Name
+    def set_add_contact(self,name, contact):
+        self.contact[name] = contact
+
+    def get_contacts(self):
+        return self.contact
+
+    def delete_contact(self, name):
+        if name in self.contact:
+            del self.contact[name]
+            return 'contact deleted'
+        else:
+            return "contact not found"
+
+    def search_contact(self, name):
+        if name in self.contact:
+            return self.contact.get(name)
+        else:
+            return "contact not found"
+
+    def edit_contact(self, name, contact):
+        for details in self.contact:
+            if details == name and contact:
+                self.contact[name] = contact
+
+
+class Messages:
+    def __init__(self):
+        self.inbox = {}
+        self.outbox = {}
+
+
+
+    def set_send_message(self, name, message):
+        self.outbox[name] = [message]
+
+    def set_receive_message(self, name, message):
+        self.inbox[name] = [message]
+
+    def get_inbox(self):
+        return self.inbox
+
+    def get_outbox(self):
+        return self.outbox
+
+    def delete_message(self, name):
+        for receiver in self.outbox:
+            if receiver == name:
+                del self.outbox[name]
+
+
+
+
+
+
+
 
 
 
--- Test/test_Nokia_3310.py
+++ Test/test_Nokia_3310.py
@@ -1,17 +1,56 @@
 import unittest
-from Nokia_3310 import PhoneBook
-
+from Nokia_3310 import *
 
 class TestNokiaPhoneBook(unittest.TestCase):
     def setUp(self):
         self.phonebook = PhoneBook()
 
-    def test_that_phonebook_add_Name_is_Empty(self):
-        name = self.phonebook.get_add_Name()
-        self.assertEqual(name, [])
+    def test_that_phonebook_is_empty(self):
+        self.assertEqual(self.phonebook.is_empty(), {})
 
-    def test_that_phonebook_Add_Name_Succesfully(self):
-        self.phonebook.set_add_Name('Mike')
-        self.assertEqual(self.phonebook.get_add_Name(), ['Mike'])
+    def test_that_phonebook_has_contact(self):
+        self.phonebook.set_add_contact('Michael', 234567 )
+        self.phonebook.set_add_contact('samuel', 56789)
+        self.phonebook.get_contacts()
+        self.assertEqual(self.phonebook.get_contacts(), {'Michael':234567, 'samuel':56789})
+
+    def test_that_phonebook_contact_can_be_deleted(self):
+        self.phonebook.set_add_contact('Michael', 234567 )
+        self.phonebook.set_add_contact('samuel', 56789)
+        self.phonebook.delete_contact('Michael')
+        self.assertEqual(self.phonebook.get_contacts(), {'samuel':56789})
+
+    def test_to_search_contact(self):
+        self.phonebook.set_add_contact('Michael', 234567 )
+        self.phonebook.set_add_contact('samuel', 56789)
+        result = self.phonebook.search_contact('Michael')
+        self.assertEqual(result, 234567)
+
+    def test_to_delete_contact(self):
+        self.phonebook.set_add_contact('Michael', 234567)
+        self.phonebook.set_add_contact('samuel', 56789)
+        result = self.phonebook.delete_contact('samuel')
+        self.assertEqual(result, 'contact deleted')
+
+class TestNokiaMessages(unittest.TestCase):
+    def setUp(self):
+        self.messages = Messages()
+
+    def test_that_message_inbox_is_empty(self):
+        self.assertEqual(self.messages.get_inbox(), {})
+
+    def test_that_message_outbox_is_empty(self):
+        self.assertEqual(self.messages.get_outbox(), {})
+
+    def test_that_message_is_sent(self):
+        self.messages.set_send_message('mike', 'how are you')
+        self.assertEqual(self.messages.get_outbox(), {'mike':['how are you']})
+
+    def test_that_phone_saves_received_messages(self):
+        self.messages.set_receive_message('sam', 'Hi there')
+        self.assertEqual(self.messages.get_inbox(), {'sam':['Hi there']})
+
+    def test_that_message_can_be_deleted(self):
+        self.messages.
 
 
